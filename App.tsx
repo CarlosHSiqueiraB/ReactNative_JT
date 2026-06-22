7d61df66792e4591536cb85146266961026881d5
CarlosHSiqueiraB <carloshenriquesiqueiraborges@gmail.com>
2026-06-22 11:28:24 -0300

    Adicionando acelerometro(imcopleto)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from 'react';
+import {Accelerometer} from 'expo-sensors';
 
 export default function App() {
+  const [leitura, setLeitura] = useState({
+    x: 0,
+    y: 0,
+    z: 0
+  });
+
+  useEffect(() => {
+    Accelerometer.setUpdateInterval(50);
+    const inscricao = Accelerometer.addListener(setLeitura);
+
+    return inscricao.remove();
+
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Macumba Saravá</Text>
